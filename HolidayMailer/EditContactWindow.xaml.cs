1a8c15292ea3d941da045954e5a8203fc5fd8bd9
Brian Mize <brian.r.mize@eagles.ewu.edu>
2017-12-06 07:29:27 -0800

    Fixed crashing bug when selecting blank row to edit or remove

--- HolidayMailer/EditContactWindow.xaml.cs
+++ HolidayMailer/EditContactWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -25,37 +26,40 @@ namespace HolidayMailer {
             this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
 
-        private void Button_save_Click(object sender, RoutedEventArgs e) {
-            if (IsValid()) {
-                DataRowView row = (DataRowView)dataGrid_contacts.SelectedItems[0];
-                string oldFName = row["FirstName"].ToString();
-                string oldLName = row["LastName"].ToString();
+        private void Button_save_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsValid())
+            {
+                DataRowView row = dataGrid_contacts.SelectedItems[0] as DataRowView;
                 string oldEmail = row["Email"].ToString();
-                bool oldRecv = (bool)row["RecievedMail"];
                 string newFname = textBox_fname.Text;
                 string newLname = textBox_lName.Text;
                 string newEmail = textBox_email.Text;
-                bool newRecv = (bool)checkBox_recieved_email.IsChecked;
+                bool newRecv = (bool) checkBox_recieved_email.IsChecked;
                 db.ExecuteDbQuery(Queries.UpdateContactRecord(oldEmail, newFname, newLname, newEmail, newRecv));
                 db.ExecuteDbQuery(Queries.UpdateMemberRecordByEmail(oldEmail, newEmail));
                 db.LoadDataGrid(dataGrid_contacts, Queries.SelectAll(Database.ContactsTable));
+                ClearTextBoxes();
             }
-            else {
-                MessageBox.Show(Application.Current.MainWindow, "Please select a contact to edit or click close to return to main window.", "Contact Selection");
+            else
+            {
+                MessageBox.Show(Application.Current.MainWindow, "Please be sure all fields are filled out and that the email address is valid or click close to return to main window.", "Contact Selection");
                 this.Focus();
             }
         }
 
         private bool IsValid() {
-            bool valid = IsValidTextBox(textBox_fname);
-            valid = IsValidTextBox(textBox_lName);
-            valid = IsValidTextBox(textBox_email);
-            return valid;
-
+            return IsValidTextBox(textBox_fname) && IsValidTextBox(textBox_lName) && IsValidEmail(textBox_email);
         }
 
         private bool IsValidTextBox(TextBox tb) {
             return !string.IsNullOrEmpty(tb.Text);
+        }
+
+        private bool IsValidEmail(TextBox email)
+        {
+            Regex rgx = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            return rgx.IsMatch(email.Text) ? true : false;
         }
 
         private void LoadNodes() {
@@ -63,8 +67,10 @@ namespace HolidayMailer {
         }
 
         private void DataGrid_contacts_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            if (dataGrid_contacts.SelectedItems.Count > 0) {
-                DataRowView row = (DataRowView)dataGrid_contacts.SelectedItems[0];
+
+            DataRowView row = dataGrid_contacts.SelectedItems[0] as DataRowView;
+
+            if (row != null && row[0] != null) {
                 string oldFName = row["FirstName"].ToString();
                 string oldLName = row["LastName"].ToString();
                 string oldEmail = row["Email"].ToString();
@@ -74,6 +80,13 @@ namespace HolidayMailer {
                 textBox_email.Text = oldEmail;
                 checkBox_recieved_email.IsChecked = oldRecv;
             }
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox_email.Text = "";
+            textBox_fname.Text = "";
+            textBox_lName.Text = "";
         }
 
         private void Button_cancel_Click(object sender, RoutedEventArgs e) {
